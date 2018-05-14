ec167efc0b341a0742876fa1036c38d143a42498
Jason Renaud <renaudjj@g.cofc.edu>
2018-05-14 11:52:12 -0600

    Added 2 context text to verify results of POST

--- spec/unit/app/api_spec.rb
+++ spec/unit/app/api_spec.rb
@@ -1,1 +1,16 @@
 require_relative '../../../app/api'
+
+module ExpenseTracker
+    RSpec.describe 'POST /expenses' do
+        context 'when the expense is successfully recorded' do
+            it 'returns the expense id'
+            it 'responds with a 200 (OK)'
+        
+        context 'when the expense fails validation' do
+            it 'returns an error message'
+            it 'responds with a 422 (Unprocessable entity)'
+        end
+
+        end 
+    end
+end
