35324db90cae8c5f6a458d46895b20ef77c3ef98
Jason Renaud <renaudjj@g.cofc.edu>
2018-05-27 12:40:15 -0600

    Adding in Ledger class. Will add Mongo DB connectivity

---
+++ app/ledger.rb
@@ -0,0 +1,10 @@
+module ExpenseTracker
+    RecordResult = Strut.new(:success?, :expense_id, :error_message)
+
+    class Ledger
+        def initialize
+            
+        end
+    end
+    
+end
