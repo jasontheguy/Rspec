c30da33b04d1b5f1cd1d8c5dbf17359ea8d88723
Jason Renaud <renaudjj@g.cofc.edu>
2018-05-27 12:40:33 -0600

    Adding in Ledger class. Will add Mongo DB connectivity

--- app/api.rb
+++ app/api.rb
@@ -1,5 +1,6 @@
 require 'sinatra/base'
 require 'json'
+require_relative 'ledger'
 
 module ExpenseTracker #Module ~ Namespaces
         class API < Sinatra::Base
@@ -12,7 +13,12 @@ module ExpenseTracker #Module ~ Namespaces
             post'/expenses' do #POST method mapped to expenses
             expense =JSON.parse(request.body.read) #POST call to expenses generates JSON K,V 
             result = @ledger.record(expense)
-            JSON.generate('expense_id'=> result.expense_id)    
+            if result.success?
+            JSON.generate('expense_id'=> result.expense_id)
+            else
+                status 422
+                JSON.generate('error'=> result.error_message)
+            end        
         end
 
             get '/expenses/:date' do
